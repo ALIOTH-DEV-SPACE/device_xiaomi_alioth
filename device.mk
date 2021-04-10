raystef66/device_xiaomi_lmi_cr7

lmi: Implement powehal from LA.UM.8.12.r1-11900-sm8250.0
Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

device.mk
@@ -133,6 +133,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.2-service.lmi
+
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
 

power/Android.mk
@@ -1,0 +1,61 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.power@1.2-service.lmi
+LOCAL_MODULE_TAGS  := optional
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_STEM := android.hardware.power@1.2-service
+
+LOCAL_CFLAGS += \
+    -Wno-unused-parameter \
+    -Wno-unused-variable
+
+LOCAL_C_INCLUDES := \
+    external/libxml2/include \
+    external/icu/icu4c/source/common
+
+LOCAL_HEADER_LIBRARIES += \
+    libhardware_headers \
+    libutils_headers
+
+LOCAL_SRC_FILES := \
+    hint-data.c \
+    list.c \
+    metadata-parser.c \
+    power-common.c \
+    Power.cpp \
+    powerhintparser.c \
+    service.cpp \
+    utils.c
+
+LOCAL_REQUIRED_MODULES := \
+    android.hardware.power@1.2-service.lmi.rc
+
+LOCAL_SHARED_LIBRARIES := \
+    android.hardware.power@1.2 \
+    libbase \
+    libcutils \
+    libdl \
+    libhidlbase \
+    libhidltransport \
+    liblog \
+    libutils \
+    libxml2
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.power@1.2-service.lmi.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
+LOCAL_MODULE_STEM := android.hardware.power@1.2-service.rc
+
+LOCAL_SRC_FILES := android.hardware.power@1.2-service.rc
+
+include $(BUILD_PREBUILT)

ALIOTH-DEV-SPACE/device_xiaomi_alioth

device.mk
@@ -127,6 +127,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.2-service.alioth
+
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
 

power/Android.mk
@@ -1,0 +1,61 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.power@1.2-service.alioth
+LOCAL_MODULE_TAGS  := optional
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_STEM := android.hardware.power@1.2-service
+
+LOCAL_CFLAGS += \
+    -Wno-unused-parameter \
+    -Wno-unused-variable
+
+LOCAL_C_INCLUDES := \
+    external/libxml2/include \
+    external/icu/icu4c/source/common
+
+LOCAL_HEADER_LIBRARIES += \
+    libhardware_headers \
+    libutils_headers
+
+LOCAL_SRC_FILES := \
+    hint-data.c \
+    list.c \
+    metadata-parser.c \
+    power-common.c \
+    Power.cpp \
+    powerhintparser.c \
+    service.cpp \
+    utils.c
+
+LOCAL_REQUIRED_MODULES := \
+    android.hardware.power@1.2-service.alioth.rc
+
+LOCAL_SHARED_LIBRARIES := \
+    android.hardware.power@1.2 \
+    libbase \
+    libcutils \
+    libdl \
+    libhidlbase \
+    libhidltransport \
+    liblog \
+    libutils \
+    libxml2
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.power@1.2-service.alioth.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
+LOCAL_MODULE_STEM := android.hardware.power@1.2-service.rc
+
+LOCAL_SRC_FILES := android.hardware.power@1.2-service.rc
+
+include $(BUILD_PREBUILT)