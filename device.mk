raystef66/device_xiaomi_lmi_cr7

lmi: import CAF powerhal from LA.UM.8.9.r1-08300-SM6xx.0
  * Build it to vendor_overlay

Signed-off-by: Akhil Narang <me@akhilnarang.dev>
Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

device.mk
@@ -75,6 +75,10 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.2-service.lmi
+
 # Telephony
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true

power/Android.mk
@@ -1,0 +1,36 @@
+LOCAL_PATH := $(call my-dir)
+
+
+# HAL module implemenation stored in
+# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.power@1.2-service.lmi
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_STEM := android.hardware.power@1.2-service
+
+LOCAL_REQUIRED_MODULES := android.hardware.power@1.2-service.rc
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libhidlbase libhidltransport libutils android.hardware.power@1.2
+LOCAL_HEADER_LIBRARIES += libutils_headers
+LOCAL_HEADER_LIBRARIES += libhardware_headers
+LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c service.cpp Power.cpp
+LOCAL_C_INCLUDES := external/libxml2/include \
+                    external/icu/icu4c/source/common
+
+# Include target-specific files.
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.power@1.2-service.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
+LOCAL_MODULE_STEM :=  android.hardware.power@1.2-service.rc
+LOCAL_SRC_FILES := android.hardware.power@1.2-service.rc
+include $(BUILD_PREBUILT)
+

ALIOTH-DEV-SPACE/device_xiaomi_alioth

device.mk
@@ -69,6 +69,10 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.2-service.alioth
+
 # Telephony
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true

power/Android.mk
@@ -1,0 +1,36 @@
+LOCAL_PATH := $(call my-dir)
+
+
+# HAL module implemenation stored in
+# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.power@1.2-service.alioth
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_STEM := android.hardware.power@1.2-service
+
+LOCAL_REQUIRED_MODULES := android.hardware.power@1.2-service.rc
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libhidlbase libhidltransport libutils android.hardware.power@1.2
+LOCAL_HEADER_LIBRARIES += libutils_headers
+LOCAL_HEADER_LIBRARIES += libhardware_headers
+LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c service.cpp Power.cpp
+LOCAL_C_INCLUDES := external/libxml2/include \
+                    external/icu/icu4c/source/common
+
+# Include target-specific files.
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.power@1.2-service.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
+LOCAL_MODULE_STEM :=  android.hardware.power@1.2-service.rc
+LOCAL_SRC_FILES := android.hardware.power@1.2-service.rc
+include $(BUILD_PREBUILT)
+