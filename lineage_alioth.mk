8cff4122e1b5b2331f0fe1fc3f2c2efc5ffd012d
jomadeto <jomadeto@gmail.com>
2021-05-18 17:03:23 +0200

    alioth: Move from common to device tree

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -4,11 +4,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_alioth
@@ -20,5 +27,5 @@ TARGET_SCREEN_DENSITY:=450
 
 
 TARGET_BOOT_ANIMATION_RES := 1080
-
+TARGET_GAPPS_ARCH := arm64
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
