3bc32a0722eff5e359b41b91300b33346dad6e27
jomadeto <jomadeto@gmail.com>
2021-05-18 16:58:26 +0200

    alioth: set vendor sec patch level

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Security patch level
+VENDOR_SECURITY_PATCH := 2021-03-01
+
 DEVICE_PATH := device/xiaomi/alioth
 
 # Inherit from sm8250-common
@@ -17,13 +20,9 @@ TARGET_OTA_ASSERT_DEVICE := alioth
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
-# Prebuilt Core
-
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/alioth
 TARGET_KERNEL_CONFIG := vendor/alioth_defconfig
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-
-
