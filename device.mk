94b039b8a531124dd1b9ba732741b0c21f8e2faa
xyyx <xyyx@mail.ru>
2021-05-31 08:50:44 +0300

    alioth: Fix Mifare Classic reading fixed Mifare Classic reading with LEGACY_MIFARE_READER=1
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- device.mk
+++ device.mk
@@ -29,6 +29,10 @@ PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
      $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
      $(LOCAL_PATH)/keylayout/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc
+	 
+# NFC
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
