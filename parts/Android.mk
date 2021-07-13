f465010d3c375715cf94cf91c3d0444f4d5c086f
GtrCraft <nickvanbokhorst@hotmail.com>
2021-07-13 08:38:16 +0200

    alioth: Remove doze from parts
    * We use crdroid's doze
    
    Change-Id: Ic6b7523e03f1e37969f1ea68f5aaeedcdda4e53b

--- parts/Android.mk
+++ parts/Android.mk
@@ -33,8 +33,6 @@ LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
     $(TOP)/packages/resources/devicesettings/res
 
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
-
 include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
